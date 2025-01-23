2d378c571f86f32c38cd15102166479ffe141268
Cezar Nogueira <cezartv1221@gmail.com>
2025-01-23 11:41:01 -0300

    Fix: Resolver a exibição dos textos na tela

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
 let numeroSecreto = gerarNumeroAleatorio();
 
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo do número secreto';
-
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número entre 1 e 10'
-
 function exibirTexto(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
 
-function gerarNumeroAleatorio() {
-    return ParseInt(Math.random() * 10 + 1);
+exibirTexto('h1', 'Jogo do número secreto');
+exibirTexto('p', 'Escolha um número entre 1 e 10');
+
+function verificarChute() {
+    console.log(numeroSecreto);
 }
 
-exibirTexto('h1', 'Jogo do número secreto')
-exibirTexto('p', 'Escolha um número entre 1 e 10')
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10 + 1);
+}
